27b125b2b4cee27f9a1f84d53b03dab314914ad7
Faiz Khatri <faizkhatri00@gmail.com>
2024-09-10 09:53:18 +0530

    remove no-store cache to generate content while build

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,9 @@
 import { API_URL } from "@/utils/contants";
 import WhyChooseUs from "@/components/WhyChooseUsSection";
 
-export const dynamic = "force-dynamic";
-
 async function getOptions() {
   const response = await fetch(
-    `${API_URL}/why-choose-uses?populate[image][fields][0]=url`,
-    {
-      cache: "no-store",
-    }
+    `${API_URL}/why-choose-uses?populate[image][fields][0]=url`
   );
   const result = await response.json();
   return result.data;
